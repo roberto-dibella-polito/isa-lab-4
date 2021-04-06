e89dd4e7ae2f8a12bd799eed4d0c3c6319fd1013
roberto-dibella-polito <roberto.dibella@studenti.polito.it>
2021-04-06 19:09:11 +0200

    Commented files for better conversations

--- adder/src/dut_if.sv
+++ adder/src/dut_if.sv
@@ -1,3 +1,9 @@
+/*****************************************************************
+	DUT Interfaces
+	- port_in: INTERNAL interface, connecting the instatiation to the DUT itself
+	- port_out: EXTERNAL interface, connecting the DUT to the test
+******************************************************************/
+
 interface dut_if(input clk, rst);
     logic [31:0] A, B;
     logic [31:0] data;
--- adder/tb/driver.sv
+++ adder/tb/driver.sv
@@ -5,15 +5,22 @@ class driver extends uvm_driver #(packet_in);
     input_vif vif;
     event begin_record, end_record;
 
+	// Constructor
     function new(string name = "driver", uvm_component parent = null);
         super.new(name, parent);
     endfunction
 
+	// Builds (configures) the driver
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         assert(uvm_config_db#(input_vif)::get(this, "", "vif", vif));
     endfunction
-
+	
+	/********************************************
+		RUN PHASE
+		Driver invokes its own defined methods
+		to run the test and drive the DUT
+	*********************************************/
     virtual task run_phase(uvm_phase phase);
         super.run_phase(phase);
         fork
@@ -23,6 +30,11 @@ class driver extends uvm_driver #(packet_in);
         join
     endtask
 
+	/********************************************
+		RESET_SIGNALS
+		User-defined task
+		Used to reset everything
+	*********************************************/
     virtual protected task reset_signals();
         wait (vif.rst === 1);
         forever begin
@@ -33,23 +45,35 @@ class driver extends uvm_driver #(packet_in);
         end
     endtask
 
+	/********************************************
+		GET_AND_DRIVE 
+		Waits until the reset is down, then starts to record
+	*********************************************/
     virtual protected task get_and_drive(uvm_phase phase);
-        wait(vif.rst === 1);
+        wait(vif.rst === 1);	// Waits until TRUE
         @(negedge vif.rst);
         @(posedge vif.clk);
         
         forever begin
             seq_item_port.get(req);
-            -> begin_record;
+            -> begin_record;		// TRIGGER begin_record, used to trigger record_tr (see below)
+									// it's like an ASYNCHRONOUS EVENT!
             drive_transfer(req);
         end
     endtask
 
+	/******************************************************
+		DRIVE_TRANSFER
+		Transmits data to the DUT using the vif interface
+	*******************************************************/
     virtual protected task drive_transfer(packet_in tr);
         vif.A = tr.A;
         vif.B = tr.B;
         vif.valid = 1;
-
+		
+		// Triggered at clk positive edge
+		// NOTES that here there is not forever begin:
+		// it is called every time by get_and_drive!
         @(posedge vif.clk)
         
         while(!vif.ready)
@@ -61,6 +85,12 @@ class driver extends uvm_driver #(packet_in);
         @(posedge vif.clk);
     endtask
 
+	/*****************************************************************
+		RECORD_TR
+		The method that TRULY handles the transmission
+		It works with ALWAYS: it is started by GET AND DRIVE
+		and stopped by DRIVE_TRANSFER (that is inside GET AND DRIVE)
+	******************************************************************/
     virtual task record_tr();
         forever begin
             @(begin_record);
